90713ef49a575e5412d230af642d0738bb7f23ef
maulya230 <75597705+maulya230@users.noreply.github.com>
2022-01-20 07:50:14 +0530

    Create numpy initializing with zeros.py

---
+++ numpy initializing with zeros.py
@@ -0,0 +1,18 @@
+import numpy as np
+n1=np.zeros((1,2))
+n1
+
+#output array([[0., 0.]])
+
+import numpy as np
+n1=np.zeros((5,5))
+n1
+
+'''
+output
+array([[0., 0., 0., 0., 0.],
+       [0., 0., 0., 0., 0.],
+       [0., 0., 0., 0., 0.],
+       [0., 0., 0., 0., 0.],
+       [0., 0., 0., 0., 0.]])
+ '''
