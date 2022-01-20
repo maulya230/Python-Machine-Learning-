9322d79b8d33b66d2eb923e849054dcaa1ff1c05
maulya230 <75597705+maulya230@users.noreply.github.com>
2022-01-20 08:13:21 +0530

    Update joining numpy arrays.py

--- joining numpy arrays.py
+++ joining numpy arrays.py
@@ -3,3 +3,22 @@ import numpy as np
 n1=np.array([10,0,30])
 n2=np.array([40,50,60])
 np.vstack((n1,n2))
+
+#output
+# array([[10,  0, 30],
+#        [40, 50, 60]])
+
+n1=np.array([10,0,30])
+n2=np.array([40,50,60])
+np.hstack((n1,n2))
+
+# output
+# array([10,  0, 30, 40, 50, 60])
+
+n1=np.array([10,0,30])
+n2=np.array([40,50,60])
+np.column_stack((n1,n2))
+
+# array([[10, 40],
+#        [ 0, 50],
+#        [30, 60]])
