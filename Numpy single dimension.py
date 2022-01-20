baa6346040b5c492ce19921e26d96ddfd1ad39a8
maulya230 <75597705+maulya230@users.noreply.github.com>
2022-01-20 07:45:00 +0530

    Create Numpy single dimension.py

---
+++ Numpy single dimension.py
@@ -0,0 +1,9 @@
+'''Numpy staands for numerical python and is core library for numeric and scintific computing'''
+
+#single dimensional array
+
+import numpy as np
+n1=np.array([10,20,30,40])
+n1
+
+# array([10, 20, 30, 40])
