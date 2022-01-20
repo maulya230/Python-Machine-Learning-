deab4bc5bf77753c05252a4a6ae5515173233aec
maulya230 <75597705+maulya230@users.noreply.github.com>
2022-01-20 08:04:02 +0530

    Create numpy initializing with random values.py

---
+++ numpy initializing with random values.py
@@ -0,0 +1,9 @@
+'''Initializing Numpy array with random numbers'''
+import numpy as np
+n1=np.random.randint(100,1000,10)
+n1
+
+'''
+output
+array([606, 817, 223, 935, 167, 583, 586, 378, 833, 140])
+'''
